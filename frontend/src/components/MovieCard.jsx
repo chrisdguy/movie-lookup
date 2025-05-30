1c5e1203bc34006d3c328f6876031fb189329443
Chris Guy <chrisdguy@icloud.com>
2025-05-30 11:40:58 -0400

    Added basic home page and started on favorites page.

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ function MovieCard({movie}) {
     <>
     <div className="movie-card">
       <div className="movie-poster">
-        <img src="{movie.url}" alt="{movie.title}"/>
+        <img src={movie.url} alt={movie.title}/>
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavClick}>
             ❤
@@ -24,4 +24,4 @@ function MovieCard({movie}) {
   )
 }
 
-export default MovieCard
+export default MovieCard;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,41 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "John Wock", release_date: "2021" },
+    { id: 3, title: "John Wack", release_date: "2022" },
+    { id: 4, title: "John Wuck", release_date: "2023" },
+  ];
+
+  const search = (e) => {
+    e.preventDefault()
+    alert(searchQuery);
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={search} className="seatch-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">Search</button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
