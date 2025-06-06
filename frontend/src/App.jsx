a8006794723b4c2c33876fa76400308814ec7e77
Chris Guy <chrisdguy@icloud.com>
2025-06-06 16:47:41 -0400

    Clicking the navbar-brand element or 'Home' nav-link element wil now reload the Home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 import NavBar from "./components/NavBar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div class="bg-neutral-800 min-h-screen">
       <NavBar />
       <main className="main-content">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home key={location.key}/>} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
